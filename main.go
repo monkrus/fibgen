ac49d9c0f97b0aada887c28d4151aba9673c88cf
Sergei Stadnik <sergeigodev@gmail.com>
2021-06-27 14:19:28 -0700

    second commit

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
-func main() {
-	
-}
+import "fmt"
+
 
 func fibo() func() int {
 first, second := 0, 1
 return func() int {
 	ret := first
-	first, second = second, first
+	first, second = second, first+second
 	return ret
 }
-
-
-
+}
+func main() {
+	f := fibo()
+	for i:=0; i< 10; i++ {
+		fmt.Println(f())
+	}
 }

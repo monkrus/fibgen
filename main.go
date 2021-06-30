948feb95cb6c04c81270fa903549c72e0bb811db
Sergei Stadnik <sergeigodev@gmail.com>
2021-06-30 08:45:24 -0700

    upd

--- main.go
+++ main.go
@@ -2,18 +2,19 @@ package main
 
 import "fmt"
 
-
 func fibo() func() int {
-first, second := 0, 1
-return func() int {
-	ret := first
-	first, second = second, first+second
-	return ret
-}
+	first, second := 0, 1
+	return func() int {
+		ret := first
+		first, second = second, first+second
+		return ret
+	}
+
 }
 func main() {
+
 	f := fibo()
-	for i:=0; i< 10; i++ {
+	for i := 0; i < 10; i++ {
 		fmt.Println(f())
 	}
 }

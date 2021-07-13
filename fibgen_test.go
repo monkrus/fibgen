b71f05f0297b34c81f2e1aec8c8d7d174c026ef3
Sergei Stadnik <sergeisqa@gmail.com>
2021-07-13 10:50:13 -0700

    upd

--- fibgen_test.go
+++ fibgen_test.go
@@ -3,20 +3,21 @@ package main
 import (
 	"testing"
 )
+
 // function TestFibo is using functionality of the testing package
 func TestFibo(t *testing.T) {
-// we create a struct with inputted and expected values
+	// we create a struct with inputted and expected values
 	var tests = []struct {
 		input    int
 		expected int
 	}{
-// indicate abovementioned values 
+		// indicate abovementioned values
 		{0, 0},
 		{1, 1},
 		{20, 6765},
 	}
-// run range loop  for all tests
-// where "if input is not equalt to expected output, the error is created" 
+	// run range loop  for all tests
+	// where "if input is not equalt to expected output, the error is created"
 	for _, test := range tests {
 		if output := Fibonacci(test.input); output != test.expected {
 			t.Errorf("Test Failed: {%d} inputted, {%d} expected, recieved: {%d}\n",
--- main.go
+++ main.go
@@ -2,57 +2,17 @@ package main
 
 import (
 	"fmt"
-    "log"
-    "net/http"
-	"strings"
-	"testing"
 )
-
-func TestConstruct(t *testing.T) {
-	var tests = []struct {
-		input  int
-		expected int
-		received int
-	}{
-		// input  0, received 0
-		//  input 1, received 1
-		//  input 10, received 55
-		//  input -2, received "value needs to be equal or greater than zero"
-	
-	
-
-	for _, test := range tests {
-		if output := construct(test.input, test.received); output != test.expected {
-			t.Errorf("Test Failed: {%s} inputted, {%s} expected, recieved: {%s}\n",
-				fmt.Sprint(test.input, test.received), test.expected, output)
-		}
-	}
-
-}
 
 func Fibonacci(n int) int {
 	if n == 0 || n == 1 {
 		return n
 	} else {
-		return (fibonacci(n-1) + fibonacci(n-2))
+		return (Fibonacci(n-1) + Fibonacci(n-2))
 	}
 }
 
 func main() {
-        // Set routing rules
-        http.HandleFunc("/", Tmp)
-
-        //Use the default DefaultServeMux.
-        err := http.ListenAndServe(":8080", nil)
-        if err != nil {
-                log.Fatal(err)
-        }
-}
-
-func Tmp(w http.ResponseWriter, r *http.Request) {
-        io.WriteString(w, "test")
-}
-
 
 	var n, i, j int
 	j = 0
@@ -62,13 +22,13 @@ func Tmp(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Fibonacci series: ")
 	for i = 1; i <= n; i++ {
-		fmt.Print(fibonacci(j), ",")
+		fmt.Print(Fibonacci(j), ",")
 		j++
 	}
 	fmt.Println()
 }
 
-/* option
+/* option 2
 func fibo() func() int {
 	first, second := 0, 1
 	return func() int {

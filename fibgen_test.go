87e4d306714f0c630f4e2ee477eb9240862ee32b
Sergei Stadnik <sergeisqa@gmail.com>
2021-08-04 11:10:39 -0700

    Add test

--- fibgen_test.go
+++ fibgen_test.go
@@ -29,6 +29,8 @@ func TestFibo(t *testing.T) {
 		}
 	}
 }
+
+// test to confirm the server has been started
 func TestPingRoute(t *testing.T) {
 	router := setupRouter()
 
@@ -38,4 +40,25 @@ func TestPingRoute(t *testing.T) {
 
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, "pong", w.Body.String())
+}
+func TestNum(t *testing.T) {
+	// we create a struct with inputted and expected values
+	 tests := []struct {
+		input    int
+		expected int
+		&n int 
+	}{
+		// indicate abovementioned values
+		{&n, 0},
+		{&n, 1},
+		{&n, 9223372036854775807},
+	}
+	// run range loop  for all tests
+	// where "if input is not equal to expected output, the error is created"
+	for _, test := range tests {
+		if output := Fibonacci(test.input); output != test.expected {
+			t.Errorf("Test Failed: {%d} inputted, {%d} expected, recieved: {%d}\n",
+				test.input, test.expected, output)
+		}
+	}
 }

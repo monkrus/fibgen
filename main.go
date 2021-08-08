ad76c7ad69d7480fad0c5d691bc0c1836d15b974
cyberS-El <74835012+cyberS-El@users.noreply.github.com>
2021-08-08 10:22:48 +0300

    Move handlers to setupRouter

--- main.go
+++ main.go
@@ -21,49 +21,32 @@ func setupRouter() *gin.Engine {
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
+	r.GET("/get", func(c *gin.Context) {
+		number := c.DefaultQuery("number", "0")
+		n, err := strconv.Atoi(number)
+		if err != nil {
+			c.String(
+				http.StatusBadRequest, err.Error())
+		} else {
+			if n == 0 {
+				c.String(
+					http.StatusBadRequest, "number should be a positive number, but it is zero")
+			} else if n < 0 {
+				c.String(
+					http.StatusBadRequest, "number should be a positive number, but it is negative")
+			} else if n > 9223372036854775807 {
+				c.String(
+					http.StatusBadRequest, "number should be a number, but it is exceeds the maximum value if int")
+			} else {
+				c.String(http.StatusOK, fmt.Sprintf("Result is %d", Fibonacci(n)))
+			}
+		}
+
+	})
 	return r
 }
 
 func main() {
-
-	var n, i, j int
-	j = 0
-
-	fmt.Print("Enter the number of terms: ")
-	fmt.Scanln(&n)
-
-	fmt.Println("Fibonacci series: ")
-	for i = 1; i <= n; i++ {
-		fmt.Print(Fibonacci(j), ",")
-		j++
-	}
-	fmt.Println()
-
 	r := setupRouter()
 	r.Run(":8080")
-
-	r.GET("/get", func(c *gin.Context) {
-		//number := c.DefaultQuery("number", "0")
-		//c.String(http.StatusOK, number)
-		number := c.DefaultQuery("number", "0")
-		n, err := strconv.Atoi(number)
-		if err != nil {
-			if n == 0 {
-				c.String(
-					http.StatusBadRequest, "number should be a positive number, but it is zero")
-			}
-			if n < 0 {
-				c.String(
-					http.StatusBadRequest, "number should be a positive number, but it is negative")
-			}
-			if n > 9223372036854775807 {
-				c.String(
-					http.StatusBadRequest, "number should be a number, but it is exceeds the maximum value if int")
-			}
-		} else {
-			c.String(http.StatusOK, fmt.Sprintf("Result is %d", Fibonacci(n)))
-		}
-
-	})
-
 }

f896c7de6a507263f5a98b4a3c2ca19b2f437238
Sergei Stadnik <sergeisqa@gmail.com>
2021-09-05 09:09:55 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,69 +1,43 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"strconv"
+	"math"
 
-	"github.com/gin-gonic/gin"
 	"github.com/mattn/go-colorable"
 	log "github.com/sirupsen/logrus"
 )
 
-func Fibonacci(n int) int {
-	defer handleOutOfBounds()
-	if n >= 44 {
+func Fibonacci(n int) (retValue int) {
+	defer func() {
+		if r := recover(); r != nil {
+			retValue = -1
+		}
+	}()
+	if (math.MaxInt64 / 2) < n {
 		panic("Out of bound access for function!")
 	}
 	if n <= 1 {
 		return n
 	}
-	return (Fibonacci(n-1) + Fibonacci(n-2))
+	return Fibonacci(n-1) + Fibonacci(n-2)
 }
 
-func handleOutOfBounds() {
-	if r := recover(); r != nil {
-		fmt.Println("Recovering from panic:", r)
-	}
-}
+func setupRouter() *MyGinServer {
+	r := NewMyGinServer("test name")
 
-func setupRouter() *gin.Engine {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
+	r.InitRoutes([]RouteDescriptor{
+		{Path: UrlPong, Method: Pong, MethodType: HTTPGet},
+		{Path: UrlFib, Method: Fib, MethodType: HTTPGet},
 	})
-	r.GET("/get", func(c *gin.Context) {
-		number := c.DefaultQuery("number", "0")
-		n, err := strconv.Atoi(number)
-		if err != nil {
-			c.String(
-				http.StatusBadRequest, err.Error())
-			return
-		}
-		switch {
-		case n == 0:
-			c.String(
-				http.StatusBadRequest, "number should be a positive number, but it is zero")
-		case n < 0:
-			c.String(
-				http.StatusBadRequest, "number should be a positive number, but it is negative")
-		case n == 2147483648:
-			c.String(
-				http.StatusBadRequest, "number should be a number, but it exceeds the maximum value if int")
-		default:
-			c.String(http.StatusOK, fmt.Sprintf("Result is %d", Fibonacci(n)))
-		}
 
-	})
 	return r
 }
 
 func main() {
-
-	r := setupRouter()
-	r.Run(":8080")
-	fmt.Println(Fibonacci(43))
-
+	err := setupRouter().Run(":8080")
+	if err != nil {
+		log.Fatalf("Internal error: %v", err)
+	}
 	//coloration for logging
 	log.SetFormatter(&log.TextFormatter{ForceColors: true})
 	log.SetOutput(colorable.NewColorableStdout())
@@ -75,5 +49,4 @@ func main() {
 
 	log.Warn("This is a warning")
 	log.Error("An error occured!")
-
 }
